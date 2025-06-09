96d35a874dd1549f51056a2b931f373b07505435
FaabiCarol <faabicaroline@gmail.com>
2025-06-09 20:42:52 -0300

    Ajusta ALLOWED_HOSTS para deploy no Render

--- projeto_pi_01/settings.py
+++ projeto_pi_01/settings.py
@@ -28,9 +28,8 @@ SECRET_KEY = 'django-insecure-9i(h)z3+k+7%9@$8g-c=z&wbnoj&(6urcm(u7nmo8nni&4lhsl
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['render.com','projeto-pi-01-grupo010-1.onrender.com','localhost']
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Render detecta a variável de ambiente DJANGO_SETTINGS_MODULE
 if os.getenv('RENDER'):
@@ -126,6 +125,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'core' / 'static']
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
